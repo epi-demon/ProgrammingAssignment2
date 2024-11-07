689598aa0af998b4a5afbc7f5e05b99164362982
epi-demon <mileslandrywork@gmail.com>
2024-11-06 21:14:06 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,43 @@
 ## functions do
 
 ## Write a short comment describing this function
+## This function creates a special matrix object that can cashe its inverse. It returns a list of functions to both set and get the matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+invs <- NULL
+        set <- function(y) {
+        x <<- y
+        invs <<- NULL
 }
 
-f
+get <- function() {
+        x
+}
+
+setInverse <- function(inverse) {
+        invs <<- inverse
+}
+
+getInverse <- function() {
+        invs
+}
+
+list(set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+        
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+       invs <- x$getInverse()
+if (!is.null(invs)) {
+        message( "getting cashe data")
+        return(invs)
+}
+
+mat <- x$get()
+invs <- solve(mat, ...)
+
+x$setInverse(invs)
+
+invs
 }
